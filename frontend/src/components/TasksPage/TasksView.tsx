31033e5edb572a10062c9c358479fcfa1dbf2c9c
Stel0r <65517739+Stel0r@users.noreply.github.com>
2023-07-02 20:48:52 -0500

    creacion de modal para proyectos inicial

--- frontend/src/components/TasksPage/TasksView.tsx
+++ frontend/src/components/TasksPage/TasksView.tsx
@@ -33,25 +33,6 @@ function TasksView() {
             <div className="projectsList box-border h-auto w-4/6 bg-gray-200">
                 <Outlet />
             </div>
-            {showTaskCreation &&
-                <div className="modal fixed h-full w-full top-0 left-0 bg-black bg-opacity-50 flex items-center">
-                    <div className="modal-window w-96 m-auto rounded-sm overflow-hidden">
-                        <div className="modal-head bg-gray-200 p-5 flex items-center">
-                            <span>Nueva Tarea</span>
-                            <button form="form" className="ms-auto rounded-full bg-green-400 mr-1  px-5 py-2 text-sm">Crear</button>
-                            <button onClick={() => { setshowTaskCreation(false) }} className=" rounded-full bg-green-400 px-5 py-2 text-sm">Cancelar</button>
-                        </div>
-                        <div className="modal-bod bg-white p-5">
-                            <form id="form" onSubmit={handleSubmit}>
-                                <p>Nombre</p>
-                                <input type="text" className="bg-gray-100 border-2 border-black w-full text-md p-1 rounded-lg mb-5" />
-                                <p>Descripcion</p>
-                                <input type="text" className="bg-gray-100 border-2 border-black w-full text-md p-1 rounded-lg mb-5" />
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            }
         </div>
     )
 }
