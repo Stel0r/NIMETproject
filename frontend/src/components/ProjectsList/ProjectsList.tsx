62938b4b3f40ccc4595607dd756cb6fe4b322a5c
Stel0r <65517739+Stel0r@users.noreply.github.com>
2023-09-04 11:02:29 -0500

    cambios varios

--- frontend/src/components/ProjectsList/ProjectsList.tsx
+++ frontend/src/components/ProjectsList/ProjectsList.tsx
@@ -11,7 +11,7 @@ function ProjectsList() {
 
     return (
         <div className="m-auto p-10">
-            <div className="flex">
+            <div className="flex items-center">
                 <p className="font-roboto text-2xl font-bold">Mis Proyectos</p>
                 <button className="ms-auto block rounded-full bg-green-400 px-5 py-3 text-md" onClick={() => setShowTaskCreation(true)}>Nuevo Proyecto</button>
             </div>
